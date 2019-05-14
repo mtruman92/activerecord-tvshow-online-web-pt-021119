54fb5d23e34ae4e5174ab1da77f4e32a570b4756
Mickae Truman <mtruman@nyit.edu>
2019-05-14 00:50:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def Show.most_popular_show
-    Show.where("rating = ?", Show.highest_rating)
+    Show.where("rating = ?", Show.highest_rating).first
   end
   
   def Show.lowest_rating
